686dde12ca6f6dcdda66177b517927bfc227ca6f
RAMESHYAVAGAL <rameshchandappayavagal2024@gmail.com>
2025-01-04 14:27:59 +0530

    Update Exercise14.java

--- Patterns/src/July03/Exercise14.java
+++ Patterns/src/July03/Exercise14.java
@@ -2,7 +2,7 @@ package July03;
 
 public class Exercise14 {
 public static void main(String[] args) {
-	int rows=5;
+	int rows=10;
 	for(int i=1; i<=rows; i++) {
 		for(int j=(rows+1-i); j<=rows; j++) {
 			System.out.print(j+" ");
